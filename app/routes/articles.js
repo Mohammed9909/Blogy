1e7f08f2a2c40c6dfbac310ad2f44f728c8d0c4c
Mohammed <Mohammed.ofisan@hotmail.com>
2019-12-05 14:31:07 +0300

    add article and index

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -1,49 +1,49 @@
-// Require necessary NPM packages
+// Require necessary NPM Packages
 const express = require('express');
-
-// Instantiate a Ruoter (mini app that only handles routes)
+// Require Mongoose Model for Article
+const Article = require('../models/article');
+// Instantiate a Router (mini app that only handles routes)
 const router = express.Router();
-
 /**
  * Action:      INDEX
  * Method:      GET
  * URI:         /api/articles
  * Description: Get All Articles
  */
-router.get('./api/articles', (req, res) => {
-    Article.find().then((articles)=>{
-    res.status(200).json({ message: 'Get All Articles' });
-})
+router.get('/api/articles', (req, res) => {
+  Article.find()
+  // Return all Articles as an Array
+  .then((articles) => {
+    res.status(200).json({ articles: articles });
+  })
+  // Catch any errors that might occur
+  .catch((error) => {
+    res.status(500).json({ error: error });
+  });
 });
-
 /**
- * Action:      SHOW
- * Method:      GET
- * URI:         /api/articles/:Id
- * Description: Get An Article by Article ID
- */
-
+* Action:       SHOW
+* Method:       GET
+* URI:          /api/articles/5d664b8b68b4f5092aba18e9
+* Description:  Get An Article by Article ID
+*/
 /**
  * Action:      CREATE
  * Method:      POST
  * URI:         /api/articles
  * Description: Create a new Article
- */
-
+*/
 /**
  * Action:      UPDATE
  * Method:      PATCH
- * URI:         /api/articles/:id
- * Description: Update an Article by Article ID
+* URI:          /api/articles/5d664b8b68b4f5092aba18e9
+* Description:  Update An Article by Article ID
  */
-
-
 /**
  * Action:      DESTROY
  * Method:      DELETE
- * URI:         /api/articles/:id
- * Description: Delete an Article by Article ID
+* URI:          /api/articles/5d664b8b68b4f5092aba18e9
+* Description: Delete An Article by Article ID
  */
-
-//Export the Router so we can use it in the server.js file
+// Export the Router so we can use it in the server.js file
 module.exports = router;
